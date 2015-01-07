8f79778e17d25d1d26161c29c659e95267ef2029
AR2029145 <Arosas2@student.rcc.edu>
2015-01-06 19:55:57 -0800

    Hmwk and Projects

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony M. Rosas
+ * Created on January 5, 2015, 10:18 AM
+ * Purpose: First Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    cout<<"Hello World\n";
+    //Exit stage right!
+    return 0;
+}
+
---
+++ Hmwk/HelloWorld2/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony M. Rosas
+ * Created on January 5, 2015, 10:18 AM
+ * Purpose: First Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Prompt for input
+    cout<<"input an integer"<<endl;
+    //input the integer
+    int x;
+    cin>>x;
+    //output the input
+    cout<<"the input was x = "<<x<<endl;
+    //exit stage right!
+    return 1;}
